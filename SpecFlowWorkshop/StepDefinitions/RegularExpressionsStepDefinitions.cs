29d2fc1e8a8c4262415dc61d4d38ee79aa6c1aee
Bas Dijkstra <bas@ontestautomation.com>
2022-02-17 09:34:37 +0100

    Add exercises for using regular expressions

---
+++ SpecFlowWorkshop/StepDefinitions/RegularExpressionsStepDefinitions.cs
@@ -0,0 +1,74 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowWorkshop.StepDefinitions
+{
+    [Binding]
+    public class RegularExpressionsStepDefinitions
+    {
+        [Given(@"I have an empty basket")]
+        public void GivenIHaveAnEmptyBasket()
+        {
+        }
+
+        [Given(@"I am a collector of flags")]
+        public void GivenIAmACollectorOfFlags()
+        {
+        }
+
+        [Given(@"I have an empty fruit bowl")]
+        public void GivenIHaveAnEmptyFruitBowl()
+        {
+        }
+
+        [When(@"I put (.*) apples in my basket")]
+        public void WhenIPutApplesInMyBasket(int numberOfApples)
+        {
+        }
+
+        [When(@"I create a collection of flags from the Benelux countries")]
+        public void WhenICreateACollectionOfFlagsFromTheBeneluxCountries()
+        {
+        }
+
+        [When(@"I throw in an apple")]
+        public void WhenIThrowInAnApple()
+        {
+        }
+
+        [When(@"I throw in a banana")]
+        public void WhenIThrowInABanana()
+        {
+        }
+
+        [When(@"I throw in a pear")]
+        public void WhenIThrowInAPear()
+        {
+        }
+
+        [Then(@"I should add a flag from The Netherlands")]
+        public void ThenIShouldAddAFlagFromTheNetherlands()
+        {
+        }
+
+        [Then(@"I should add a flag from Belgium")]
+        public void ThenIShouldAddAFlagFromBelgium()
+        {
+        }
+
+        [Then(@"I should add a flag from Luxembourg")]
+        public void ThenIShouldAddAFlagFromLuxembourg()
+        {
+        }
+
+        [Then(@"I have (.*) apples in my basket")]
+        public void ThenIHaveApplesInMyBasket(int numberOfApples)
+        {
+        }
+
+        [Then(@"I have a filled fruit bowl")]
+        public void ThenIHaveAFilledFruitBowl()
+        {
+        }
+    }
+}
